488bd514b6dd4d410116eec183c3c2f2489abbd5
Kuldeep Mohanty <devops.kuldeep915@gmail.com>
2025-04-16 12:44:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "kuldeep5"
+vpc_security_group_ids = ["sg-076b5fd367827df1f"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins" , "Tomcat-1", "Tomcat-2", "Monitoring server"]
 }
