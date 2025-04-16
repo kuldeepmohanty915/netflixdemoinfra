554c5fa66cd5af5c719c4baf40bdd7e7da1e20e1
Kuldeep Mohanty <devops.kuldeep915@gmail.com>
2025-04-16 16:19:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins" , "Tomcat-1", "Tomcat-2", "Monitoring server"]
+default = ["jenkins" , "Tomcat-1", "Tomcat-2", "Pegion"]
 }
