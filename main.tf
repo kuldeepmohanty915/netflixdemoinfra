ea12c11e90d0fcb1672f74ef9aee7d1a8e5f1870
Kuldeep Mohanty <devops.kuldeep915@gmail.com>
2025-04-24 19:04:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
+count = 6
 ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.medium"
 key_name = "kuldeep5"
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins" , "Tomcat-1", "Tomcat-2", "Pegion"]
+default = ["ANSIBLE" , "DEV-1", "DEV-2", "TEST-1", "TEST-2", "MONITORING SERVER"]
 }
